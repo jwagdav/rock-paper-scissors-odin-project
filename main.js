2cc141a4b7d6a008ccbc119690cfc85638953357
jwagdav <jwagdav@gmail.com>
2022-03-11 19:42:02 -0800

    Having trouble making the PlayGame function properly return results

--- main.js
+++ main.js
@@ -1,89 +1,48 @@
+let computerSelection = computerTurn();
+let playerSelection = PlayerChoice();
+
 function computerTurn() {
 
-    const computerChoice = Math.ceil(Math.random() * 3);
-
-    if(computerChoice === 1) {
-        return "rock";
-    } else if (computerChoice === 2) {
-        return "paper";
-    } else if (computerChoice === 3) {
-        return "scissors";
-    }
-}
-
-function playerChoice() {
-    const playerInput = Window.prompt("Choose rock, paper, or scissors");
+    let computerSelection = Math.ceil(Math.random() * 3);
     
-    if(playerInput.toLowerCase() == "rock") {
-        return "rock";
-    } else if(playerInput.toLowerCase() == "paper") {
-        return "paper";
-    } else if (playerInput.toLowercase() == "scissors") {
-        return "scissors";
-    } else {
-        alert("Please choose rock, paper, or scissors");
-        return undefined;
-    }
-}
-
-function TrackScore() {
-
-    PlayRound();
-    let playerScore = 0;
-    let computerScore = 0;
-    let numOfTies = 0;
-
-    if(PlayRound() == "win") {
-        playerScore++;
-    } else if(PlayRound() == "loss") {
-        computerScore++;
-    } else {
-        numOfTies++;
+    if(computerSelection=== 1) {
+        computerSelection = "rock";
+    } else if (computerSelection === 2) {
+        computerSelection = "paper";
+    } else if (computerSelection === 3) {
+        computerSelection ="scissors";
     }
 
-    console.log(`The score is: Player: ${playerScore} Computer: ${computerScore} Tie: ${numOfTies}`);
+    return computerSelection;
 }
 
-function PlayRound(playerChoice, computerTurn) {
-    
+function PlayerChoice() {
+    const playerSelection = window.prompt("Choose rock, paper, or scissors").toLowerCase();
+    return playerSelection;
+}
 
-    switch (playerSelection) {
-        case "rock" :
-            if(computerSelection == "rock") {
-                return "tie";
-            } else if (computerSelection == "paper") {
-                return "loss";
-            } else {
-                return "win";
-            }
+console.log(playerSelection, computerSelection);
 
-        break;
+function PlayRound(playerSelection, computerSelection) {
 
-        case "paper" :
-            if(computerSelection == "rock") {
-                return "win";
-            } else if (computerSelection == "paper") {
-                return "tie";
-            } else {
-                return "loss";
-            }
-        break;
-
-        case "scissors" :
-            if(computerSelection == "rock") {
-                return "loss";
-            } else if (computerSelection == "paper") {
-                return "win";
-            } else {
-                return "tie";
-            }
-        break;
-
-        default : 
-            return "tie";
-        
+    if(playerSelection === "rock" && computerSelection === "paper") {
+        console.log("loss");
+    }else if(playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("win");
+    }else if(playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("loss");
+    }else if(playerSelection === "paper" && computerSelection === "rock") {
+        console.log("win");
+    }else if(playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("win");
+    }else if(playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("loss");
+    } else{
+        console.log("tie");
     }
     
-    PlayRound();
-    TrackScore();
 }
+
+PlayRound();
+
+
